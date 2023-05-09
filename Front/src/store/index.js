0f06c6fa4252f3b33baaf3c957fb0a4830900b13
ben <benjamin.ollier@etu.unice.fr>
2023-05-09 13:35:46 +0200

    store maj user

--- Front/src/store/index.js
+++ Front/src/store/index.js
@@ -2,17 +2,34 @@ import { createStore } from "vuex"
 const store = createStore({
     state () {
       return {
-        token: ''
+        idUser: null,
+        username: null,
+        email: null,
       }
     },
     mutations: {
-      setToken (state, token) {
-        state.token = token
+      setIdUser (state, idUser) {
+        state.idUser=idUser
+      },
+      setEmailUser (state, email) {
+        state.email=email
+      },
+      setUsernameUser (state, username) {
+        state.username=username
       }
     },
     getters: {
       getToken(state) {
         return state.token
+      },
+      getIdUser(state) {
+        return state.idUser
+      },
+      getUsername(state) {
+        return state.username
+      },
+      getEmail(state) {
+        return state.email
       }
     }
 
